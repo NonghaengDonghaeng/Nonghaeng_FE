17a2cb2100d98dce95ada662c02258f578e1c69c
김선인 <kimsunin@gimseon-in-ui-MacBookAir.local>
2024-03-01 21:14:24 +0900

    feat/footer:프레임 제작

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -1,9 +1,24 @@
+import Link from "next/link";
 import styles from "./.module.css";
+import { footerHref } from "@/storage/href";
 
 export default function Footer() {
+  const mainLink = footerHref.map((item, index) => (
+    <Link href={item.href} key={index}>
+      {item.title}
+    </Link>
+  ));
   return (
     <footer id="footer">
-      <div className={styles.footer_inner}></div>
+      <div className={styles.footer_inner}>
+        <div></div>
+        <hr></hr>
+        <div>
+          {mainLink}
+          <p>사업자 소개</p>
+          <p>저작권 관련 글</p>
+        </div>
+      </div>
     </footer>
   );
 }
