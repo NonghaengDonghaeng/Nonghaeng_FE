9b2f3458b3ca13bc4661fc4df1ed0413b8d07375
김선인 <jumto10@naver.com>
2024-03-11 15:45:39 +0900

    feat/lodg:프레임 제작중

---
+++ src/app/pages/trip/lodg/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useState } from "react";
+import Nav from "@/components/layout/nav/nav";
+import { tripHref } from "@/storage/href";
+import ScDetail from "@/components/common/search/scdetail/scdetail";
+import Overlay from "@/components/common/overlay/overlay";
+import styles from "./.module.css";
+
+export default function page() {
+  const [isClick, setIsClick] = useState(false);
+  return (
+    <>
+      <Nav href={tripHref} />
+      <main id="main">
+        <section className={styles.lodg_main}>
+          <div>
+            <h1>농촌관광</h1>
+            <ScDetail setIsClick={setIsClick} />
+            <Overlay isClick={isClick} />
+          </div>
+          <hr></hr>
+          <article></article>
+        </section>
+      </main>
+    </>
+  );
+}
