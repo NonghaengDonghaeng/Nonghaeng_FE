4502f517e581a3fb8137c12879edb214ced90b48
김선인 <jumto10@naver.com>
2024-03-24 15:37:50 +0900

    feat/detail:체험상세페이지 프레임 제작 완료
    -section1 이미지 article완성
    -section1 체험 설명 article완성
    -예약하기, 좋아요를 컴포넌트로 구분하려고 구상중

--- src/app/pages/trip/exp/detail/page.tsx
+++ src/app/pages/trip/exp/detail/page.tsx
@@ -1,24 +1,110 @@
+"use client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import useMove from "@/hooks/useMove";
+import DetailNav from "@/components/common/detailnav/detailnav";
 import styles from "./.module.css";
 import exp_detail from "@/db/expdata/detail.json";
+import grade_img from "img/tour/grade_img.png";
+import participant_img from "img/exp/participant.png";
+import time_img from "img/exp/time.png";
+import price_img from "img/exp/price.png";
+import great_img from "img/exp/great.png";
+import reservation_img from "img/exp/reservation.png";
 
 export default function page() {
+  const { element, moveElement } = useMove();
+
+  const [pageState, setPageState] = useState({
+    img_url: exp_detail.main_img_url,
+  });
+
+  useEffect(() => {
+    // api요청
+  }, []);
+
   const imgList = exp_detail.sub_img_url.map((item, index) => (
-    <li key={index}>
-      <img src={item} />
+    <li
+      key={index}
+      onClick={() => setPageState({ ...pageState, img_url: item })}
+    >
+      <img
+        src={item}
+        className={`${
+          pageState.img_url == item ? styles.img_on : styles.img_off
+        }`}
+      />
     </li>
   ));
   return (
     <main id="main">
       <section className={styles.section1}>
         <article>
-          <img src={exp_detail.main_img_url} />
+          <img src={pageState.img_url} />
           <ul>{imgList}</ul>
         </article>
         <article>
-          <h1></h1>
+          <span>
+            <Link
+              href={`/pages/trip/tour/detail?tour_id=${exp_detail.tour_info.tourId}`}
+            >
+              {exp_detail.tour_info.tourName}
+            </Link>
+
+            {" > "}
+            {exp_detail.experience_name}
+          </span>
+          <h1>
+            {exp_detail.experience_name}
+            <span>
+              <Image src={grade_img} alt="grade_img" />
+              {exp_detail.grade.toFixed(1)}
+            </span>
+          </h1>
+          <hr />
+          <p>
+            <Image src={participant_img} alt="participant_img" />
+            <label>참여인원</label>
+            {exp_detail.min_participant}명{" ~ "}
+            {exp_detail.max_participant}명
+          </p>
+          <p>
+            <Image src={time_img} alt="time_img" />
+            <label>소요시간</label>
+            {exp_detail.duration_hours}시간
+          </p>
+          <p>
+            <Image src={price_img} alt="price_img" />
+            <label>체험비용</label>
+            {exp_detail.price}원
+          </p>
+          <ul>
+            <li onClick={() => console.log("좋아요 api")}>
+              좋아요
+              <Image src={great_img} alt="great_img" />
+            </li>
+            <li>
+              예약하기
+              <Image src={reservation_img} alt="reservation_img" />
+            </li>
+          </ul>
         </article>
       </section>
-      <section className={styles.section2}></section>
+      <section className={styles.section2}>
+        <article ref={element[0]}>
+          <DetailNav moveElement={moveElement} nowRef={"기본정보"} />
+          기본정보
+        </article>
+        <article ref={element[1]}>
+          <DetailNav moveElement={moveElement} nowRef={"여행후기"} />
+          여행후기
+        </article>
+        <article ref={element[2]}>
+          <DetailNav moveElement={moveElement} nowRef={"고객문의"} />
+          문의
+        </article>
+      </section>
     </main>
   );
 }
