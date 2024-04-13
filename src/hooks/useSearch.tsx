28381f9c383de7b3691894b9633bb0420819ccad
김선인 <jumto10@naver.com>
2024-04-13 22:03:42 +0900

    feat/useSearch: trip maping완료

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -1,24 +1,21 @@
 import { useRouter } from "next/navigation";
-import { pageStateType } from "@/types/pageStateType";
 
 type propsType = {
   searchItem: {
     search_word?: string;
-    region?: string[] | string;
-    category?: string[] | string;
+    region?: string;
+    category?: string;
   };
 };
 
 export function useSearch() {
   const router = useRouter();
 
-  // const tripMap: { ["관광"]: string; ["체험"]: string; ["숙박"]: string } = {
-  //   ["관광"]: "tour",
-  //   ["체험"]: "exp",
-  //   ["숙박"]: "lodg",
-  // };
-
-  // const returnTripMap = ()
+  const tripMap: any = {
+    관광: "tour",
+    체험: "exp",
+    숙박: "lodg",
+  };
 
   /**custom search
    * @param region:스트링타입의 지역정보
@@ -34,49 +31,17 @@ export function useSearch() {
         `/pages/search?search_word=${searchItem.search_word}`
       );
     }
+    // 카테고리o, 지역 ?
+    else if (searchItem.category) {
+      router.push(
+        `/pages/trip/${tripMap[searchItem.category]}?${
+          searchItem.region !== "지역선택" && `region=${searchItem.region}`
+        }&search_word=${searchItem.search_word}`
+      );
+    }
     // 카테고리x
-    else if (searchItem.category == "유형선택") {
-      alert("유형을 선택해주세요");
-    }
-    //  else {
-    //   router.push(
-    //     `/pages/${tripMap[searchItem.category]}?region=${
-    //       searchItem.region
-    //     }&search_word=${searchItem.search_word}`
-    //   );
-    // }
-    // 지역x, 카테고리 o
-    else if (
-      searchItem.region == "지역선택" &&
-      searchItem.category !== "유형선택"
-    ) {
-      if (searchItem.category == "관광") {
-        router.push(`/pages/trip/tour?search_word=${searchItem.search_word}`);
-      }
-      if (searchItem.category == "체험") {
-        router.push(`/pages/trip/exp?search_word=${searchItem.search_word}`);
-      }
-      if (searchItem.category == "숙박") {
-        router.push(`/pages/trip/lodg?search_word=${searchItem.search_word}`);
-      }
-    }
-    // 지역o, 카테고리o
     else {
-      if (searchItem.category == "관광") {
-        router.push(
-          `/pages/trip/tour?region=${searchItem.region}&search_word=${searchItem.search_word}`
-        );
-      }
-      if (searchItem.category == "체험") {
-        router.push(
-          `/pages/trip/exp?region=${searchItem.region}&search_word=${searchItem.search_word}`
-        );
-      }
-      if (searchItem.category == "숙박") {
-        router.push(
-          `/pages/trip/lodg?region=${searchItem.region}&search_word=${searchItem.search_word}`
-        );
-      }
+      alert("유형을 선택해주세요");
     }
   }
 
