dd5af344bdc279c71fcbec11bc9167950d45867e
김선인 <jumto10@naver.com>
2024-04-19 15:28:18 +0900

    feat/getLodginState:로그인 상태관리 구현중

--- src/api/getLoginStateApi.tsx
+++ src/api/getLoginStateApi.tsx
@@ -2,14 +2,17 @@ import axios from "axios";
 import store from "@/redux/loginStateStore";
 
 export const verityJwtApi = async () => {
-  try {
-    let token = localStorage.getItem("jwt");
-    const res = await axios.get("https://nonghaeng.duckdns.org/test/jwt", {
-      headers: { Authorization: token },
-    });
-    store.dispatch({ type: "LOGIN" });
-  } catch (err) {
-    console.log(err);
-    store.dispatch({ type: "LOGOUT" });
+  let token = localStorage.getItem("jwt");
+  if (token) {
+    try {
+      const res = await axios.get("https://nonghaeng.duckdns.org/test/jwt", {
+        headers: { Authorization: token },
+      });
+      store.dispatch({ type: "LOGIN" });
+    } catch (err) {
+      alert("세션이 만료되었습니다.");
+      store.dispatch({ type: "LOGOUT" });
+      localStorage.removeItem("jwt");
+    }
   }
 };
