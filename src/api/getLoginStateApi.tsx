07310934b47b2b9f240b00f6d35076ca78c5b16c
김선인 <jumto10@naver.com>
2024-04-19 16:32:38 +0900

    feat/getLoginStateApi:로그인 상태관리 구현중

--- src/api/getLoginStateApi.tsx
+++ src/api/getLoginStateApi.tsx
@@ -10,7 +10,7 @@ export const VerifyJwtApi = async () => {
       const res = await axios.get("https://nonghaeng.duckdns.org/valid", {
         headers: { Authorization: token },
       });
-      if (res.data.valid) {
+      if (res.status == 200) {
         console.log("검증완료, 로그인 유지");
         store.dispatch({ type: "LOGIN" });
       } else {
