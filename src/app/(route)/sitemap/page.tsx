77f58ed0a47a6eb2ae17aade8713d88e4bc8985f
김선인 <jumto10@naver.com>
2024-04-19 10:52:27 +0900

    feat/sitemap: 프레임 및 Link태그 추가

--- src/app/(route)/sitemap/page.tsx
+++ src/app/(route)/sitemap/page.tsx
@@ -1,3 +1,51 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
 export default function Page() {
-  return <>사이트맵</>;
+  return (
+    <section className={styles.sitemap}>
+      <article>
+        <div>
+          <span>
+            <Link href="/intro">농행동행</Link>
+          </span>
+          <Link href="/intro/nonghaeng">- 농행동행 소개</Link>
+          <Link href="/intro/tour">- 농촌관광 소개</Link>
+        </div>
+        <div>
+          <span>
+            <Link href="/trip">농촌여행</Link>
+          </span>
+          <Link href="/trip/tour">- 농촌관광</Link>
+          <Link href="/trip/exp">- 농촌체험</Link>
+          <Link href="/trip/lodg">- 농촌숙박</Link>
+        </div>
+        <div>
+          <span>
+            <Link href="/item">기획상품</Link>
+          </span>
+        </div>
+      </article>
+      <article>
+        <div>
+          <span>
+            <Link href="/commu">커뮤니티</Link>
+          </span>
+          <Link href="/commu/notice">- 공지사항</Link>
+          <Link href="/commu/review">- 농행후기</Link>
+          <Link href="/commu/inquiry">- 고객문의</Link>
+        </div>
+        <div>
+          <span>
+            <Link href="/mypage">마이페이지</Link>
+          </span>
+          <Link href="/mypage/reserve">- 최근예약</Link>
+          <Link href="/mypage/edit">- 회원정보수정</Link>
+        </div>
+        <div>
+          <span>사이트맵</span>
+        </div>
+      </article>
+    </section>
+  );
 }
