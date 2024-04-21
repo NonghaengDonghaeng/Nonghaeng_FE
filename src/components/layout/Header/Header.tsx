b6b8c93d1690f2ae5720214b133560d645e9f10e
김선인 <jumto10@naver.com>
2024-04-21 11:49:28 +0900

    feat/Header:useMediaQuery적용중

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,7 +4,8 @@ import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { useChange } from "@/hooks/useChange";
 import { useSearch } from "@/hooks/useSearch";
-import styles from "./Header.module.css";
+import webStyles from "./styles/web.module.css";
+import mobileStyles from "./styles/mobile.module.css";
 import { headerHref } from "../../../../public/href/href";
 import Nonghaeng_Ic from "icon/nonghaeng.svg";
 import Sitemap_gray_Ic from "icon/sitemap_gray.svg";
@@ -19,14 +20,21 @@ import { useMediaQuery } from "react-responsive";
 type SubMenuType = { href: string; title: string };
 
 function Header() {
-  const isMobile = useMediaQuery({
-    query: "(max-width : 767px)",
-  });
   const change = useChange();
   const search = useSearch();
   const pathName = usePathname();
 
+  const [mobile, setMobile] = useState(false);
   const [loginState, setLoginState] = useState(false);
+  const [searchItem, setSearchItem] = useState<pageStateType>({
+    search_word: "",
+  });
+
+  const isMobile = useMediaQuery({
+    query: "(max-width : 767px)",
+  });
+
+  useEffect(() => setMobile(isMobile), [isMobile]);
 
   useEffect(() => {
     verifyJwtApi().then(() => {
@@ -42,10 +50,6 @@ function Header() {
     store.dispatch({ type: "LOGOUT" });
   }
 
-  const [searchItem, setSearchItem] = useState<pageStateType>({
-    search_word: "",
-  });
-
   function onSubmit(e: formType) {
     e.preventDefault();
     search({ searchItem: searchItem });
@@ -53,7 +57,7 @@ function Header() {
 
   const [isHover, setIsHover] = useState(false);
   const subMenuList = (subMenu: SubMenuType[]) => (
-    <ul className={`${styles.subMenu} ${isHover && styles.on}`}>
+    <ul className={`${webStyles.subMenu} ${isHover && webStyles.on}`}>
       {subMenu.map((item, index) => (
         <li key={index}>
           <Link href={item.href}>{item.title}</Link>
@@ -70,54 +74,56 @@ function Header() {
 
   return (
     <header id="header">
-      <div className={styles.header_inner}>
+      <div
+        className={mobile ? mobileStyles.header_mobile : webStyles.header_inner}
+      >
         <Link href="/">
           <Nonghaeng_Ic />
         </Link>
-        {!isMobile && (
-          <ul
-            onMouseOver={() => setIsHover(true)}
-            onMouseLeave={() => setIsHover(false)}
-          >
-            {mainMenuList}
-          </ul>
+        {!mobile && (
+          <>
+            <ul
+              onMouseOver={() => setIsHover(true)}
+              onMouseLeave={() => setIsHover(false)}
+            >
+              {mainMenuList}
+            </ul>
+            <form onSubmit={onSubmit}>
+              <input
+                onChange={(e: inputType) =>
+                  change({
+                    changeItem: searchItem,
+                    setChangeItem: setSearchItem,
+                    e,
+                  })
+                }
+                placeholder="알고 싶은 정보가 있으세요?"
+                name="search_word"
+              ></input>
+              <button type="submit">
+                <Search_gray_Ic />
+              </button>
+            </form>
+            <div>
+              {loginState ? (
+                <button onClick={logout}>로그아웃</button>
+              ) : (
+                <Link href="/acount/login">로그인</Link>
+              )}
+              <Link href={loginState ? "/mypage" : "/acount/login"}>
+                마이페이지
+              </Link>
+              <Link href="/sitemap">
+                <Sitemap_gray_Ic />
+              </Link>
+            </div>
+            <div
+              className={`${webStyles.subMenu_bg} ${isHover && webStyles.on}`}
+              onMouseOver={() => setIsHover(true)}
+              onMouseLeave={() => setIsHover(false)}
+            ></div>
+          </>
         )}
-
-        <form className={styles.header_search} onSubmit={onSubmit}>
-          <input
-            onChange={(e: inputType) =>
-              change({
-                changeItem: searchItem,
-                setChangeItem: setSearchItem,
-                e,
-              })
-            }
-            placeholder="알고 싶은 정보가 있으세요?"
-            name="search_word"
-          ></input>
-          <button type="submit">
-            <Search_gray_Ic />
-          </button>
-        </form>
-        <div>
-          {loginState ? (
-            <button onClick={logout}>로그아웃</button>
-          ) : (
-            <Link href="/acount/login">로그인</Link>
-          )}
-
-          <Link href={loginState ? "/mypage" : "/acount/login"}>
-            마이페이지
-          </Link>
-          <Link href="/sitemap">
-            <Sitemap_gray_Ic />
-          </Link>
-        </div>
-        <div
-          className={`${styles.subMenu_bg} ${isHover && styles.on}`}
-          onMouseOver={() => setIsHover(true)}
-          onMouseLeave={() => setIsHover(false)}
-        ></div>
       </div>
     </header>
   );
