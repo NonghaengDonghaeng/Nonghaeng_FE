b5f4c1f518227a1ae8847a7b7324abf617ac600f
김선인 <jumto10@naver.com>
2024-04-21 14:15:52 +0900

    feat/components:컴포넌트부터 mediquery작업 진행

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import styles from "./Footer.module.css";
 import { footerHref } from "../../../../public/href/href";
+import Home_Ic from "icon/home.svg";
+import Menu_Ic from "icon/sitemap_gray.svg";
+import Search_Ic from "icon/search_gray.svg";
+import Person_Ic from "icon/person_gray.svg";
+import Great_Ic from "icon/great_gray.svg";
 
 export default function Footer() {
   const mainLink = footerHref.map((item, index) => (
@@ -18,6 +23,23 @@ export default function Footer() {
           <p>사업자 소개</p>
           <p>저작권 관련 글</p>
         </div>
+        <ul>
+          <li>
+            <Menu_Ic />
+          </li>
+          <li>
+            <Search_Ic />
+          </li>
+          <li>
+            <Home_Ic />
+          </li>
+          <li>
+            <Person_Ic />
+          </li>
+          <li>
+            <Great_Ic />
+          </li>
+        </ul>
       </div>
     </footer>
   );
--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,8 +4,7 @@ import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { useChange } from "@/hooks/useChange";
 import { useSearch } from "@/hooks/useSearch";
-import webStyles from "./styles/web.module.css";
-import mobileStyles from "./styles/mobile.module.css";
+import styles from "./Header.module.css";
 import { headerHref } from "../../../../public/href/href";
 import Nonghaeng_Ic from "icon/nonghaeng.svg";
 import Sitemap_gray_Ic from "icon/sitemap_gray.svg";
@@ -15,7 +14,6 @@ import { pageStateType } from "@/types/pageStateType";
 import { formType } from "@/types/eventType";
 import store from "@/redux/loginStateStore";
 import { verifyJwtApi } from "@/api/loginApi";
-import { useMediaQuery } from "react-responsive";
 
 type SubMenuType = { href: string; title: string };
 
@@ -24,17 +22,10 @@ function Header() {
   const search = useSearch();
   const pathName = usePathname();
 
-  const [mobile, setMobile] = useState(false);
   const [loginState, setLoginState] = useState(false);
   const [searchItem, setSearchItem] = useState<pageStateType>({
     search_word: "",
   });
-
-  const isMobile = useMediaQuery({
-    query: "(max-width : 767px)",
-  });
-
-  useEffect(() => setMobile(isMobile), [isMobile]);
 
   useEffect(() => {
     verifyJwtApi().then(() => {
@@ -57,7 +48,7 @@ function Header() {
 
   const [isHover, setIsHover] = useState(false);
   const subMenuList = (subMenu: SubMenuType[]) => (
-    <ul className={`${webStyles.subMenu} ${isHover && webStyles.on}`}>
+    <ul className={`${styles.subMenu} ${isHover && styles.on}`}>
       {subMenu.map((item, index) => (
         <li key={index}>
           <Link href={item.href}>{item.title}</Link>
@@ -74,56 +65,50 @@ function Header() {
 
   return (
     <header id="header">
-      <div
-        className={mobile ? mobileStyles.header_mobile : webStyles.header_inner}
-      >
+      <div className={styles.header_inner}>
         <Link href="/">
           <Nonghaeng_Ic />
         </Link>
-        {!mobile && (
-          <>
-            <ul
-              onMouseOver={() => setIsHover(true)}
-              onMouseLeave={() => setIsHover(false)}
-            >
-              {mainMenuList}
-            </ul>
-            <form onSubmit={onSubmit}>
-              <input
-                onChange={(e: inputType) =>
-                  change({
-                    changeItem: searchItem,
-                    setChangeItem: setSearchItem,
-                    e,
-                  })
-                }
-                placeholder="알고 싶은 정보가 있으세요?"
-                name="search_word"
-              ></input>
-              <button type="submit">
-                <Search_gray_Ic />
-              </button>
-            </form>
-            <div>
-              {loginState ? (
-                <button onClick={logout}>로그아웃</button>
-              ) : (
-                <Link href="/acount/login">로그인</Link>
-              )}
-              <Link href={loginState ? "/mypage" : "/acount/login"}>
-                마이페이지
-              </Link>
-              <Link href="/sitemap">
-                <Sitemap_gray_Ic />
-              </Link>
-            </div>
-            <div
-              className={`${webStyles.subMenu_bg} ${isHover && webStyles.on}`}
-              onMouseOver={() => setIsHover(true)}
-              onMouseLeave={() => setIsHover(false)}
-            ></div>
-          </>
-        )}
+        <ul
+          onMouseOver={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
+          {mainMenuList}
+        </ul>
+        <form onSubmit={onSubmit}>
+          <input
+            onChange={(e: inputType) =>
+              change({
+                changeItem: searchItem,
+                setChangeItem: setSearchItem,
+                e,
+              })
+            }
+            placeholder="알고 싶은 정보가 있으세요?"
+            name="search_word"
+          ></input>
+          <button type="submit">
+            <Search_gray_Ic />
+          </button>
+        </form>
+        <div>
+          {loginState ? (
+            <button onClick={logout}>로그아웃</button>
+          ) : (
+            <Link href="/acount/login">로그인</Link>
+          )}
+          <Link href={loginState ? "/mypage" : "/acount/login"}>
+            마이페이지
+          </Link>
+          <Link href="/sitemap">
+            <Sitemap_gray_Ic />
+          </Link>
+        </div>
+        <div
+          className={`${styles.subMenu_bg} ${isHover && styles.on}`}
+          onMouseOver={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        ></div>
       </div>
     </header>
   );
