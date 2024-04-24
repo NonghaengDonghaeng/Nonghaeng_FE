ea150d0677857b2eaf3d97d901ce293d17337e57
김선인 <jumto10@naver.com>
2024-04-24 14:39:13 +0900

    feat/html: 모바일 브라우저, 데스크탑 브라우저상에서 스크롤바 숨기기

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,9 @@ const config: Config = {
         top: "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
       },
     },
+    minHeight: {
+      "real-screen": "calc(var(--vh) * 100)",
+    },
   },
   plugins: [],
 };
