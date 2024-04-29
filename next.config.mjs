4d81766620c538abe16cd5ad0fc66059c3d72932
김선인 <jumto10@naver.com>
2024-04-29 14:30:44 +0900

    fix/next.config:imgaes수정

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,9 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.chamchamtrip.com",
+        hostname: "**",
+        port: "",
+        pathname: "**",
       },
     ],
   },
