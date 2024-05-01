5b1c604668b7577db2b8e1871aaefa6992bce044
김선인 <jumto10@naver.com>
2024-05-01 16:42:24 +0900

    refact:버그수정중

--- src/app/(_route)/mypage/(api)/getMypageDataApi.tsx
+++ src/app/(_route)/mypage/(api)/getMypageDataApi.tsx
@@ -7,7 +7,7 @@ export const getMypageApi = async ({ setResData }: mypagePorpsType) => {
   console.log("마이페이지 api");
   try {
     let token = localStorage.getItem("jwt");
-    const res = await axios.get("https://nonghaeng.duckdns.org/my-page", {
+    const res = await axios.get(process.env.NEXT_PUBLIC_URL_API + "my-page", {
       headers: { Authorization: token },
     });
     setResData(await res.data);
