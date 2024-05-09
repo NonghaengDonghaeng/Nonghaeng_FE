f334fda07561cf8813c27b11872c9d4b685b95d0
김선인 <jumto10@naver.com>
2024-05-10 00:45:12 +0900

    feat/CustomImage:src가 없는 경우 기본 이미지 추가

--- src/common/components/CustomImage/CustomImage.tsx
+++ src/common/components/CustomImage/CustomImage.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import styles from "./CustomImage.module.css";
-import defaultImg from "img/Group 53 (1).png";
+import defaultImg from "img/errorImg.png";
 
 type PropsType = {
   src: string;
@@ -12,7 +12,7 @@ export default function CustomImage({ src }: PropsType) {
   const [isImgError, setIsImgError] = useState<boolean>(false);
   return (
     <Image
-      className={`${isImgError ? styles.err_img : styles.img}`}
+      className={styles.img}
       src={isImgError ? defaultImg : src}
       alt="img"
       width={800}
