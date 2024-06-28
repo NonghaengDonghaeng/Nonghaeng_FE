b6c4bded35881202cc26a8e273f28b4a1cf59261
kimsunin <suninkim10@gmail.com>
2024-06-28 19:10:28 +0900

    feat/join: 회원가입 페이지 제작
    - 회원가입 메인
    - 회원가입 소비자
    - 회원가입 판매자

--- src/app/(_route)/acount/(_route)/join/seller/page.tsx
+++ src/app/(_route)/acount/(_route)/join/seller/page.tsx
@@ -37,71 +37,73 @@ export default function Page() {
   ));
   return (
     <section className={styles.join_seller}>
-      <h1>판매자 회원가입</h1>
-      <hr />
-      <form onSubmit={onSubmitSeller}>
-        <div
-          onChange={(e: inputType) =>
-            change({
-              changeItem: seller,
-              setChangeItem: setSeller,
-              e,
-            })
-          }
-        >
-          <span># 회원정보</span>
-          <div>
-            <label>이름</label>
-            <input name="name" placeholder="이름을 입력하세요." />
+      <article>
+        <h1>판매자 회원가입</h1>
+        <hr />
+        <form onSubmit={onSubmitSeller}>
+          <div
+            onChange={(e: inputType) =>
+              change({
+                changeItem: seller,
+                setChangeItem: setSeller,
+                e,
+              })
+            }
+          >
+            <span># 회원정보</span>
+            <div>
+              <label>이름</label>
+              <input name="name" placeholder="이름을 입력하세요." />
+            </div>
+            <div>
+              <label>지역</label>
+              <select name="area_code">
+                <option value="">지역을 선택하세요.</option>
+                {regionList}
+              </select>
+            </div>
+            <div>
+              <label>이메일</label>
+              <input name="email" placeholder="이메일을 입력하세요." />
+            </div>
+            <div>
+              <label>비밀번호</label>
+              <input name="password" placeholder="비밀번호를 입력하세요." />
+            </div>
+            <div>
+              <label>비밀번호 확인</label>
+              <input
+                name="checkPassword"
+                placeholder="비밀번호를 다시 입력하세요."
+              />
+            </div>
+            <span># 마을정보</span>
+            <div>
+              <label>전화번호</label>
+              <input name="phone_number" placeholder="전화번호를 입력하세요." />
+            </div>
+            <div>
+              <label>사업자번호</label>
+              <input
+                name="business_number"
+                placeholder="사업자번호를 입력하세요."
+              />
+            </div>
+            <div>
+              <label>주소</label>
+              <input name="address" placeholder="주소를 입력하세요." />
+            </div>
+            <div>
+              <label>문의번호</label>
+              <input
+                name="call_number"
+                placeholder="문의 전화번호를 입력해주세요."
+              />
+            </div>
           </div>
-          <div>
-            <label>지역</label>
-            <select name="area_code">
-              <option value="">지역을 선택하세요.</option>
-              {regionList}
-            </select>
-          </div>
-          <div>
-            <label>이메일</label>
-            <input name="email" placeholder="이메일을 입력하세요." />
-          </div>
-          <div>
-            <label>비밀번호</label>
-            <input name="password" placeholder="비밀번호를 입력하세요." />
-          </div>
-          <div>
-            <label>비밀번호 확인</label>
-            <input
-              name="checkPassword"
-              placeholder="비밀번호를 다시 입력하세요."
-            />
-          </div>
-          <span># 마을정보</span>
-          <div>
-            <label>전화번호</label>
-            <input name="phone_number" placeholder="전화번호를 입력하세요." />
-          </div>
-          <div>
-            <label>사업자번호</label>
-            <input
-              name="business_number"
-              placeholder="사업자번호를 입력하세요."
-            />
-          </div>
-          <div>
-            <label>주소</label>
-            <input name="address" placeholder="주소를 입력하세요." />
-          </div>
-          <div>
-            <label>문의번호</label>
-            <input
-              name="call_number"
-              placeholder="문의 전화번호를 입력해주세요."
-            />
-          </div>
-        </div>
-        <button type="submit">회원가입</button>
-      </form>
+          <button type="submit">회원가입</button>
+        </form>
+      </article>
     </section>
   );
 }
--- src/app/(_route)/acount/(_route)/join/user/page.tsx
+++ src/app/(_route)/acount/(_route)/join/user/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { useState } from "react";
-import {useRouter} from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useChange } from "@/hooks/useChange";
 import styles from "./page.module.css";
 import { regionMap } from "@/model/name/name";
 import { formType, inputType } from "@/common/types/eventType";
 import { userType } from "../../../(types)/userType";
 import { joinUserApi } from "@/app/(_route)/acount/(api)/joinApi";
-import {router} from "next/client";
+import { router } from "next/client";
 
 export default function Page() {
   const router = useRouter();
@@ -24,8 +24,8 @@ export default function Page() {
 
   function onSubmitUser(e: formType) {
     e.preventDefault();
-    joinUserApi({user}).then((res) => {
-      if(res?.status == 200) {
+    joinUserApi({ user }).then((res) => {
+      if (res?.status == 200) {
         alert(res.data);
         router.push("/acount/login");
       }
@@ -38,41 +38,42 @@ export default function Page() {
     </option>
   ));
   return (
-      <section className={styles.join_user}>
+    <section className={styles.join_user}>
+      <article>
         <h1>소비자 회원가입</h1>
-        <hr/>
+        <hr />
         <form onSubmit={onSubmitUser}>
           <div
-              onChange={(e: inputType) =>
-                  change({changeItem: user, setChangeItem: setUser, e})
-              }
+            onChange={(e: inputType) =>
+              change({ changeItem: user, setChangeItem: setUser, e })
+            }
           >
             <div>
               <label>아이디</label>
-              <input name="username" placeholder="이메일을 입력하세요."/>
+              <input name="username" placeholder="이메일을 입력하세요." />
             </div>
             <div>
               <label>비밀번호</label>
-              <input name="password" placeholder="비밀번호를 입력하세요."/>
+              <input name="password" placeholder="비밀번호를 입력하세요." />
             </div>
             <div>
               <label>비밀번호 확인</label>
               <input
-                  name="check_password"
-                  placeholder="비밀번호를 다시 입력하세요."
+                name="check_password"
+                placeholder="비밀번호를 다시 입력하세요."
               />
             </div>
             <div>
               <label>이름</label>
-              <input name="name" placeholder="이름을 입력하세요."/>
+              <input name="name" placeholder="이름을 입력하세요." />
             </div>
             <div>
               <label>이메일</label>
-              <input name="email" placeholder="이메일을 입력하세요."/>
+              <input name="email" placeholder="이메일을 입력하세요." />
             </div>
             <div>
               <label>전화번호</label>
-              <input name="number" placeholder="전화번호를 입력하세요."/>
+              <input name="number" placeholder="전화번호를 입력하세요." />
             </div>
             <div>
               <label>지역</label>
@@ -84,6 +85,7 @@ export default function Page() {
           </div>
           <button type="submit">회원가입</button>
         </form>
-      </section>
+      </article>
+    </section>
   );
 }
