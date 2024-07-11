f7ad32d3f644597a046a301f4ecf60f644f169bf
kimsunin <suninkim10@gmail.com>
2024-07-12 01:28:45 +0900

    fix:test git action

--- src/app/(components)/ThemeProvider/ThemeProvider.tsx
+++ src/app/(components)/ThemeProvider/ThemeProvider.tsx
@@ -7,6 +7,7 @@ import { type ThemeProviderProps } from 'next-themes/dist/types';
 function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   const [isMount, setMount] = useState(false)
 
+
   useEffect(() => {
     setMount(true)
   }, []);
